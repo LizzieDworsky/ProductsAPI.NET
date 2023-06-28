b811255edc6be0d7700f40bd1aa5fcaadb4f2660
LizzieDworsky <amydworsky15@gmail.com>
2023-06-28 15:11:24 -0600

    added project starter code and set up inital pieces, including Product and Review models, Id only, and Controllers + migrated to database

---
+++ ProductsWebAPI/ProductsWebAPI/Models/Product.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProductsWebAPI.Models
+{
+    public class Product
+    {
+        [Key]
+        public int Id { get; set; }
+    }
+}
---
+++ ProductsWebAPI/ProductsWebAPI/Models/Review.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProductsWebAPI.Models
+{
+    public class Review
+    {
+        [Key]
+        public int Id { get; set; }
+    }
+}
